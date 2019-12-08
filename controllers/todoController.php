8acbc5d39c2dc0a98825e57d8f5fc9771474594c
ViniciusRR1 <vinicius_rr1@hotmail.com>
2019-12-08 16:07:14 -0300

    Implementando as funções do todoController

--- controllers/todoController.php
+++ controllers/todoController.php
@@ -4,6 +4,44 @@ class todoController extends Controller{
 
     public function __construct(){
         parent::__construct();
+        $t = new Tarefas();
     }
     public function index(){}
+
+
+    public function listar(){
+        $array = array();
+
+        $array = $t->listar();
+
+        header("Content-Type: application/json");
+
+        echo json_encode($array);
+    }
+
+    public function add(){
+        if(isset($_POST['titulo']) && !empty($_POST['titulo'])){
+            $titulo = addslashes($_POST['titulo']);
+
+            $t->addTarefa($titulo);
+        }
+    }
+
+    public function del(){
+        if(isset($_POST['id']) && !empty($_POST['id'])) {
+            $id = addslashes($_POST['id']);
+
+            $t->delTarefa($id);
+        }
+    }
+
+    public function update(){
+        if(isset($_POST['id']) && !empty($_POST['id'])) {
+            $id = addslashes($_POST['id']);
+            $status = addslashes($_POST['status']);
+
+            $t = new Tarefas();
+            $t->updateStatus($status, $id);
+        }
+    }
 }
